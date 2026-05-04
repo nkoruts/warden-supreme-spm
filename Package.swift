50c218c96aaf9a608304691126aec00c4461b4af
Nikita Koruts <nikitakoruts@gmail.com>
2026-05-04 17:38:48 +0300

    Update supreme version

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WardenSupreme",
-            url: "https://github.com/nkoruts/warden-supreme-kmp-wrapper/releases/download/v0.3.0/WardenSupreme.xcframework.zip",
-            checksum: "0c0855090b0c7ec47512402cbf0a1cc98d134723f722d9968bdcd75db5f98620"
+            url: "https://github.com/nkoruts/warden-supreme-kmp-wrapper/releases/download/v0.3.1/WardenSupreme.xcframework.zip",
+            checksum: "9a57721b4eda7b74d81060649e145ff4242a92ecf17279ca0a814625c345be8d"
         )
     ]
 )
